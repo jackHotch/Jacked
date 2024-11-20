8ac87e86a56a6ba8ba65da97c08ed58c0c89b602
jackHotch <jhotchkiss2021@gmail.com>
2024-11-20 18:35:10 -0500

    refactored resume version to work with new navbar

--- apps/gym/src/app/(user)/profile/page.tsx
+++ apps/gym/src/app/(user)/profile/page.tsx
@@ -5,7 +5,7 @@ import styles from './Profile.module.css'
 import { toast } from '@gymapp/gymui/Toast'
 
 const Profile = () => {
-  return <div className={styles.container}>Profile Page</div>
+  return <h1 className={styles.heading}>Profile - To Be Finished...</h1>
 }
 
 export default Profile
