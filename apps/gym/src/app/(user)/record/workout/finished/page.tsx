279c254e6856c6e514e5352362af0b38d52ebaa5
jackHotch <jhotchkiss2021@gmail.com>
2025-09-13 18:04:30 -0400

    updated workouts to v3

--- apps/gym/src/app/(user)/record/workout/finished/page.tsx
+++ apps/gym/src/app/(user)/record/workout/finished/page.tsx
@@ -11,7 +11,7 @@ const Finished = () => {
 
   return (
     <div className={styles.created}>
-      <p>Workout #{workout?.count + 1} Recorded!</p>
+      <p>Workout #{workout?.data.count + 1} Recorded!</p>
       <Button.Primary
         size='xlarge'
         sx={{ marginTop: '30px' }}
--- apps/gym/src/hooks/api/useCreateWorkout.ts
+++ apps/gym/src/hooks/api/useCreateWorkout.ts
@@ -1,15 +1,9 @@
 import { useMutation } from '@tanstack/react-query'
 import { createWorkout } from '@/actions/workout'
 import { IWorkout } from '@/types'
-import { useRouter } from 'next/navigation'
 
 export const useCreateWorkout = () => {
-  const router = useRouter()
-
   return useMutation({
     mutationFn: (workout: IWorkout[]) => createWorkout(workout),
-    onSuccess: () => {
-      router.push('/record/workout/finished')
-    },
   })
 }
