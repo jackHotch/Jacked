1d40eaf92e471728d4d989650e574f4e1513ec66
jackHotch <jhotchkiss2021@gmail.com>
2025-09-14 21:56:22 -0400

    created toast function in gymui

---
+++ packages/gymui/src/components/Toast/toast.tsx
@@ -0,0 +1,26 @@
+import styles from './toast.module.css'
+import { toast as sonnerToast } from 'sonner'
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded'
+
+type toastType = 'default' | 'success' | 'error' | 'loading'
+
+export const toast = (message: string, type: toastType = 'default') => {
+  switch (type) {
+    case 'success':
+      return sonnerToast(
+        <div className={styles.success}>
+          <CheckCircleRoundedIcon color='success' />
+          {message}
+        </div>
+      )
+    case 'error':
+      return sonnerToast(
+        <div className={styles.error}>
+          <CheckCircleRoundedIcon color='error' />
+          {message}
+        </div>
+      )
+    case 'default':
+      return sonnerToast(message)
+  }
+}
