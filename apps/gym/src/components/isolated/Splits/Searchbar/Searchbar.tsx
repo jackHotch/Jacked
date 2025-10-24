d04ac53f67c3b524519e28509996bf9afbff2feb
jackHotch <jhotchkiss2021@gmail.com>
2025-10-23 23:36:11 -0400

    working on split cards

--- apps/gym/src/components/isolated/Splits/Searchbar/Searchbar.tsx
+++ apps/gym/src/components/isolated/Splits/Searchbar/Searchbar.tsx
@@ -5,27 +5,20 @@ import { SplitSearchbarProps, TextInputChangeEvent } from '@/types'
 import { useState, useRef } from 'react'
 import SearchIcon from '@mui/icons-material/Search'
 import { CloseIcon } from '@gymapp/gymui/CloseIcon'
-import { useOutsideClick } from '@/hooks'
 
-export const Searchbar = ({ splits }: SplitSearchbarProps) => {
-  const [filteredData, setFilteredData] = useState([])
+export const Searchbar = ({ filterSplits }: SplitSearchbarProps) => {
   const [wordEntered, setWordEntered] = useState('')
   const containerRef = useRef()
-  useOutsideClick(containerRef, () => setFilteredData([]))
 
   const clearInput = () => {
-    setFilteredData([])
     setWordEntered('')
+    filterSplits('')
   }
 
-  const filterSplits = (e: TextInputChangeEvent) => {
+  const handleChange = (e: TextInputChangeEvent) => {
     const searchWord = e.target.value
     setWordEntered(searchWord)
-    const newFilter = splits?.filter((value) => {
-      return value.name.toLowerCase().includes(searchWord.toLowerCase())
-    })
-    if (searchWord === '') setFilteredData([])
-    else setFilteredData(newFilter)
+    filterSplits(searchWord)
   }
 
   return (
@@ -36,28 +29,10 @@ export const Searchbar = ({ splits }: SplitSearchbarProps) => {
           type='text'
           placeholder='Find a Split'
           value={wordEntered}
-          onChange={filterSplits}
+          onChange={handleChange}
         />
         <div className={styles.search_icon}>{!wordEntered ? <SearchIcon /> : <CloseIcon onClick={clearInput} />}</div>
       </div>
-      {filteredData.length > 0 && (
-        <div className={styles.list_container}>
-          {filteredData.map((split, i) => {
-            return (
-              <div className={styles.list_item}>
-                <div>
-                  <span>{split.name}</span>
-                  {split.is_active ? <span> *</span> : null}
-                </div>
-                <div className={styles.list_item_details}>
-                  <span>{split.total_template_days} days per week</span>
-                  <span>{split.total_template_exercises} total exercises</span>
-                </div>
-              </div>
-            )
-          })}
-        </div>
-      )}
     </div>
   )
 }
