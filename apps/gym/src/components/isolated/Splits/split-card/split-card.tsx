9fd7858027cb7b3077de11c7fc858a1c59cf016a
jackHotch <jhotchkiss2021@gmail.com>
2025-10-25 18:42:43 -0400

    worked on styling of card and layout

--- apps/gym/src/components/isolated/Splits/split-card/split-card.tsx
+++ apps/gym/src/components/isolated/Splits/split-card/split-card.tsx
@@ -1,42 +1,45 @@
-import { Card } from '@gymapp/gymui/Card'
 import styles from './split-card.module.css'
 import { SplitCardProps } from '@/types'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
 import dayjs from 'dayjs'
 import { motion } from 'motion/react'
 
 export const SplitCard = ({ split }: SplitCardProps) => {
-  const MotionCard = motion.create(Card)
   const createdAtDisplay = dayjs(split.created_at).format('MMM D, YYYY')
 
   return (
-    <motion.div className={styles.container} whileHover={{ scale: 1.02 }}>
-      <Card className={`${split.is_active ? styles.active_split : null}`}>
-        <Card.Content>
-          <div className={styles.header}>
-            <span className={styles.name}>
-              {split.name}
-              {split.is_active ? <span className={styles.active_tag}>Active</span> : null}
-            </span>
-            <ChevronRightIcon />
-          </div>
+    <motion.div
+      className={`${styles.container} ${split.is_active ? styles.active_split : null}`}
+      whileHover={{ scale: 1.02 }}
+    >
+      <div className={styles.header}>
+        <span className={styles.name}>
+          {split.name}
+          {split.is_active ? <span className={styles.active_tag}>Active</span> : null}
+        </span>
+        <ChevronRightIcon sx={{ color: 'var(--primary)' }} />
+      </div>
 
-          <div className={styles.bottom_row}>
-            <span>
-              <span>{split.total_template_days}</span> days per week
-            </span>
-            <span>{split.total_template_exercises} total exercises</span>
-          </div>
+      <div className={styles.split_attributes}>
+        <span className={styles.days_per_week}>
+          <CalendarTodayIcon />
+          <span>{split.total_template_days} days</span>
+        </span>
+        <span className={styles.total_workouts}>
+          <FitnessCenterIcon />
+          <span>{split.total_workouts_completed} workouts</span>
+        </span>
+      </div>
 
-          <div className={styles.footer}>
-            <hr className={styles.hr} />
-            <div className={styles.footer_content}>
-              <span className={styles.last_used}>Last used: {split.last_used_display}</span>
-              <span>Created {createdAtDisplay}</span>
-            </div>
-          </div>
-        </Card.Content>
-      </Card>
+      <div className={styles.footer}>
+        <hr className={styles.hr} />
+        <div className={styles.footer_content}>
+          <span>Last used: {split.last_used_display}</span>
+          <span>Created {createdAtDisplay}</span>
+        </div>
+      </div>
     </motion.div>
   )
 }
