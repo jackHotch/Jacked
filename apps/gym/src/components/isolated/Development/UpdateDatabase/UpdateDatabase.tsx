ef18dd259a4e9340a56b488d86602c442c65e35b
jackHotch <jhotchkiss2021@gmail.com>
2026-01-11 00:55:39 -0500

    added another section for db scripts to dev page

---
+++ apps/gym/src/components/isolated/Development/UpdateDatabase/UpdateDatabase.tsx
@@ -0,0 +1,14 @@
+import styles from './UpdateDatabase.module.css'
+
+export function UpdateDatabase() {
+  return (
+    <div>
+      <h4>Update Database:</h4>
+
+      <ul className={styles.list}>
+        <li>Update Workouts Script</li>
+        <li>Update Weight Script</li>
+      </ul>
+    </div>
+  )
+}
