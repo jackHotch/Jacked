119d3487708dc5f35ddc7074d2b01da0f5003623
jackHotch <jhotchkiss2021@gmail.com>
2026-01-12 20:19:06 -0500

    created common css styles

--- apps/gym/src/components/isolated/Development/Caching/Caching.tsx
+++ apps/gym/src/components/isolated/Development/Caching/Caching.tsx
@@ -1,10 +1,11 @@
 import { CachingProps } from '@/types'
+import sectionStyles from '../Section.module.css'
 import styles from './Caching.module.css'
 import { Switch } from '@gymapp/gymui/Switch'
 
 export function Caching({ handleChange, updatedSettings }: CachingProps) {
   return (
-    <div className={styles.settings_section}>
+    <div className={`${styles.settings_section} ${sectionStyles.section}`}>
       <h4>Caching:</h4>
       <ul className={styles.settings_list}>
         <li>
--- apps/gym/src/components/isolated/Development/UpdateDatabase/UpdateDatabase.tsx
+++ apps/gym/src/components/isolated/Development/UpdateDatabase/UpdateDatabase.tsx
@@ -1,8 +1,9 @@
+import sectionStyles from '../Section.module.css'
 import styles from './UpdateDatabase.module.css'
 
 export function UpdateDatabase() {
   return (
-    <div>
+    <div className={sectionStyles.section}>
       <h4>Update Database:</h4>
 
       <ul className={styles.list}>
